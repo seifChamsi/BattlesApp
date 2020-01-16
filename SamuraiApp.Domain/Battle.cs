2150585d96c9cedc7b37023d30ff691865d8d691
Saifeddin Chamsi <Saifeddin.Chamsi@sousse.r-iset.tn>
2020-01-16 15:20:59 +0100

    setting Models

--- SamuraiApp.Domain/Battle.cs
+++ SamuraiApp.Domain/Battle.cs
@@ -6,7 +6,8 @@ namespace SamuraiApp.Domain
 {
     public class Battle
     {
-        public int Id { get; set; }
+
+        pqublic int Id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
