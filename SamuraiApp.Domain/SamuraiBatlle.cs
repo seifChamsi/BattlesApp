97c3078703f86099e208c6a02b069ee29d6bacf5
Saifeddin Chamsi <Saifeddin.Chamsi@sousse.r-iset.tn>
2020-01-16 14:48:05 +0100

    Ajoutez des fichiers projet.

---
+++ SamuraiApp.Domain/SamuraiBatlle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Domain
+{
+    public class SamuraiBatlle
+    {
+        public int SamuraiId { get; set; }
+        public int BattleId { get; set; }
+        public Samurai Samurai { get; set; }
+        public Battle Battle { get; set; }
+
+    }
+}
